459940b532ebb5823af18d10ea8b3911b76f2af8
ibnuiqbal <ibnuiqbal99@gmail.com>
2018-09-14 13:46:14 +0700

    update tampil dafreg marketing

--- user/marketing/course-info.php
+++ user/marketing/course-info.php
@@ -1,7 +1,7 @@
 <?php
 include '../../config/koneksi.php';
-$query = mysqli_query($koneksi, "SELECT * FROM m_peserta GROUP BY peserta_instansi_nama ORDER BY peserta_id ASC");
-$query2 = mysqli_query($koneksi, "SELECT * FROM m_peserta ORDER BY peserta_id ASC");
+$query = mysqli_query($koneksi, "SELECT p.peserta_jenis, p.peserta_instansi_nama, j.jadwal_sesi, t.id_peserta, t.id_jadwal FROM m_peserta P INNER JOIN(t_sertifikasi t INNER JOIN m_jadwal j ON t.id_jadwal=j.jadwal_id) ON p.peserta_id=t.id_peserta GROUP BY p.peserta_instansi_nama ORDER BY p.peserta_id ASC");
+$query2 = mysqli_query($koneksi, "SELECT p.peserta_jenis, p.peserta_alamat, p.peserta_email, p.peserta_telp, p.peserta_instansi_nama, j.jadwal_sesi, t.id_peserta, t.id_jadwal FROM m_peserta P INNER JOIN(t_sertifikasi t INNER JOIN m_jadwal j ON t.id_jadwal=j.jadwal_id) ON p.peserta_id=t.id_peserta GROUP BY p.peserta_instansi_nama ORDER BY p.peserta_id ASC");
 ?>
 
 <div class="header">
@@ -31,10 +31,10 @@ $query2 = mysqli_query($koneksi, "SELECT * FROM m_peserta ORDER BY peserta_id AS
         <?php $no=1; ?>
         <?php while ($data = mysqli_fetch_array($query)) {
           // code...
-          $id = $data["peserta_id"];
+          $id = $data["id_peserta"];
           $jenis = $data["peserta_jenis"];
           $instansi = $data["peserta_instansi_nama"];
-          $jadwal = $data["jadwal_id"];
+          $jadwal = $data["id_jadwal"];
         ?>
 
         <tr>
@@ -91,10 +91,10 @@ $query2 = mysqli_query($koneksi, "SELECT * FROM m_peserta ORDER BY peserta_id AS
   <?php $modalloop = 1; ?>
   <?php while ($data = mysqli_fetch_array($query2)) {
     // code...
-    $id = $data["peserta_id"];
+    $id = $data["id_peserta"];
     $jenis = $data["peserta_jenis"];
     $instansi = $data["peserta_instansi_nama"];
-    $jadwal = $data["jadwal_id"];
+    $jadwal = $data["id_jadwal"];
     $email = $data["peserta_email"];
     $alamat = $data["peserta_alamat"];
     $telp = $data["peserta_telp"];
@@ -175,18 +175,19 @@ $query2 = mysqli_query($koneksi, "SELECT * FROM m_peserta ORDER BY peserta_id AS
                   </tr>
                 </thead>
                 <tbody>
-                  <?php $qmtable = mysqli_query($koneksi, "SELECT jns_pelatihan_kode, peserta_id, peserta_nama  FROM m_peserta WHERE peserta_instansi_nama = '$instansi'"); ?>
+                  <?php $qmtable = mysqli_query($koneksi, "SELECT p.peserta_nama, t.id_peserta, t.id_jns_pelatihan, jp.jns_pelatihan_nama FROM m_peserta P INNER JOIN(t_sertifikasi t INNER JOIN m_jns_pelatihan jp ON t.id_jns_pelatihan=jp.jns_pelatihan_kode) ON p.peserta_id=t.id_peserta ORDER BY jp.jns_pelatihan_kode ASC"); ?>
                   <?php if (mysqli_num_rows($qmtable)>0) { ?>
 
                     <?php while ($data = mysqli_fetch_array($qmtable)) {
                       // code...
-                      $mtable_pel = $data["jns_pelatihan_kode"];
-                      $mtable_id = $data["peserta_id"];
+                      $mtable_pel = $data["jns_pelatihan_nama"];
+                      $mtable_pel_kode = $data["id_jns_pelatihan"];
+                      $mtable_id = $data["id_peserta"];
                       $mtable_nama = $data["peserta_nama"];
                     ?>
                     <tr>
                       <td><?php echo $mtable_pel; ?></td>
-                      <td><?php echo $mtable_pel; ?></td>
+                      <td><?php echo $mtable_pel_kode; ?></td>
                       <td><?php echo $mtable_id; ?></td>
                       <td><?php echo $mtable_nama; ?></td>
                     </tr>

8632081aafddf59daf9edfbeb99bc111d1e6d058
ibnuiqbal <ibnuiqbal99@gmail.com>
2018-09-27 11:10:00 +0700

    cetak.php

---
+++ user/marketing/cetak.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> CETAK | BUKTI REGISTRASI</title>
+	<link rel="stylesheet" type="text/css" href="../../assets/css/bootstrap.css">
+</head>
+<body>
+	<div class="container">
+	<table class="table table-bordered">
+	<thead>
+		<tr>
+		<th>
+			<img src="../../assets/image/header-cetak.JPG" style="width:900px;padding-left:130px;" >
+		</th>
+		</tr>
+	</thead>
+	<tbody>
+	<tr>
+		<td>
+
+		<div style="padding-top: 30px;padding-bottom: 30px;">
+				<h4 class="text-center">
+					REGISTRATION FORM
+				</h4>
+				<h4 class="text-center">
+					NO.REG : 0001
+				</h4>
+		</div>
+
+		<div class="content text-left" style="font-size: 20px; padding-top:45px; padding-bottom:75%;padding-left:70px;">
+			<div class="row">
+				<label class="col-sm-3">
+					Nama Peserta
+				</label>
+				<div>: </div>
+				<div class="col-sm-8">
+					Andi Cahyo Nugroho Eko
+				</div>
+			</div>
+			<div class="row">
+				<label class="col-sm-3">
+					Alamat
+				</label>
+				<div>: </div>
+				<div class="col-sm-8">
+					Jl. Panjang Jiwo NO.5 Surabaya
+				</div>
+				</div>
+			<div class="row">
+				<label class="col-sm-3">
+					No. Handphone
+				</label>
+				<div>: </div>
+				<div class="col-sm-8">
+					081331473554
+				</div>
+			</div>
+			<div class="row">
+				<label class="col-sm-3">
+					Email
+				</label>
+				<div>: </div>
+				<div class="col-sm-8">
+					ibnuiqbal99@gmail.com
+				</div>
+			</div>
+			<div class="row">
+				<label class="col-sm-3">
+					Jenis Pelatihan
+				</label>
+				<div>: </div>
+				<div class="col-sm-8">
+					Zend PHP
+				</div>
+			</div>
+			<div class="row">
+				<label class="col-sm-3">
+					Jadwal
+				</label>
+				<div>: </div>
+				<div class="col-sm-8">
+					12/06/2018 s/d 20/06/2018
+				</div>
+			</div>
+			<div class="row">
+				<label class="col-sm-3">
+					Tgl, Registrasi
+				</label>
+				<div>: </div>
+				<div class="col-sm-8">
+					10/06/2018
+				</div>
+			</div>
+		</div>
+		</td>
+	</tr>
+	</tbody>
+	</table>
+	</div>
+</body>
+</html>
